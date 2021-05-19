7661b3d6f74720903b0dafb520394fb2a8128e1f
Ricardo Quintero <ricardo.qm@culiacan.tecnm.mx>
2021-05-19 10:41:26 -0600

    Init testSuma

--- app/src/test/java/wsCalculadora/AppTest.java
+++ app/src/test/java/wsCalculadora/AppTest.java
@@ -11,4 +11,11 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test public void testSuma() {
+        Calculadora c = new Calculadora();
+
+        assertTrue(c.suma(2,3)==5);
+        assertTrue(c.suma(3.1, 4.5)==7.6);
+    }
 }
