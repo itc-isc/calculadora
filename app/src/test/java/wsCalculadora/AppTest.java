f0ce9ef759b6e7081efddefa8b854297f774202e
Ricardo Quintero <ricardo.qm@culiacan.tecnm.mx>
2021-05-19 10:51:02 -0600

    Elmiinando prueba viejita

--- app/src/test/java/wsCalculadora/AppTest.java
+++ app/src/test/java/wsCalculadora/AppTest.java
@@ -7,11 +7,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
-
     @Test public void testSuma() {
         Calculadora c = new Calculadora();
 
